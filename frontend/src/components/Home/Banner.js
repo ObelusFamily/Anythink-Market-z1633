e68a1a088a26eb3891b9d080f38154b016c1d1f1
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-09-29 15:58:27 -0700

    Add an onClick to make search box appear

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
+  const [searching, setSearching] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="container">
-          <div className="row align-items-center">
-            <span id="get-part" className="col-sm-2">
+          <div className="row align-items-center justify-content-center">
+            <span
+              id="get-part"
+              className="col-sm-2 p-0"
+              onClick={() => {
+                setSearching(true);
+              }}
+            >
               A place to get
             </span>
-            <div className="col input-group">
-              <input
-                id="search-box"
-                type="search"
-                className="form-control"
-                placeholder="What is it that you truly desire?"
-                minLength="3"
-                onChange={(e) => {
-                  if (e.target.value.length >= 3) {
-                    e.preventDefault();
-                    props.search(e.target.value);
-                  }
-                }}
-              />
-              {/* <button className="btn btn-secondary" type="button" onClick={()=>{search()}} >
-                    <i className="bi bi-search"></i>
-                  </button> */}
-            </div>
-            <span className="col-sm-2"> the cool stuff.</span>
+            {searching && (
+              <div className="col input-group">
+                <input
+                  id="search-box"
+                  type="search"
+                  className="form-control"
+                  placeholder="What is it that you truly desire?"
+                  minLength="3"
+                  onChange={(e) => {
+                    if (e.target.value.length >= 3) {
+                      e.preventDefault();
+                      props.search(e.target.value);
+                    }
+                  }}
+                />
+              </div>
+            )}
+            <span className="col-sm-2 p-0"> the cool stuff.</span>
           </div>
         </div>
       </div>
